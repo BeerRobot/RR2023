53bddd0218b4a7fb5652c67a80d46de8e0c7725c
MilzyUD <milesgilbert.ud@gmail.com>
2023-07-08 01:00:24 +0100

    Little test environment with some obstacles

---
+++ Assets/Scripts/Obstacle/Move.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+
+// Move object back and forth
+public class Move : MonoBehaviour
+{
+    [SerializeField] private Transform startMarker;
+    [SerializeField] private Transform endMarker;
+
+    [SerializeField] private float speed = 1.0F;
+
+
+    // Update
+    void Update()
+    {
+        float time = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, time);
+    }
+}
