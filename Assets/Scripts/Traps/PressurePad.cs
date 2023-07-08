e3ac66bd612b9dd7009fc0d3f9b0c9dd6dab5792
MilzyUD <milesgilbert.ud@gmail.com>
2023-07-08 23:30:25 +0100

    Pressure pad wall trap

--- Assets/Scripts/Traps/PressurePad.cs
+++ Assets/Scripts/Traps/PressurePad.cs
@@ -2,20 +2,17 @@ using System.Collections;
 using UnityEngine;
 
 
-// Automatic floor spikes
-// ToDo: Trigger floor spikes with pressure pads
-public class FloorSpikes : MonoBehaviour
+// Pressure Pad wall trap
+public class PressurePad : MonoBehaviour
 {
     // Private variables
     [SerializeField] private float intervalInSeconds;
     [SerializeField] private float pushSpeed;
     [SerializeField] private float retractSpeed;
 
-    [SerializeField] private GameObject spikeObject;
+    [SerializeField] private GameObject wall;
     [SerializeField] private Vector3 startPosition;
     [SerializeField] private Vector3 endPosition;
-
-    [SerializeField] private MeshCollider trapCollider;
 
     private bool isPush = false;
     private bool isRetract = false;
@@ -25,57 +22,58 @@ public class FloorSpikes : MonoBehaviour
     private float journeyLength;
 
 
-    // Start
-    private void Start()
-    {
-        StartCoroutine(PushTrap());
-    }
-
-
-    // Update 
-    void Update()
+    // Update
+    private void Update()
     {
         if (isPush)
         {
             float distCovered = (Time.time - startTime) * speed;
             float fractionOfJourney = distCovered / journeyLength;
-            spikeObject.transform.localPosition = Vector3.Lerp(startPosition, endPosition, fractionOfJourney);
+            wall.transform.localPosition = Vector3.Lerp(startPosition, endPosition, fractionOfJourney);
         }
-        
+
         if (isRetract)
         {
             float distCovered = (Time.time - startTime) * speed;
             float fractionOfJourney = distCovered / journeyLength;
-            spikeObject.transform.localPosition = Vector3.Lerp(endPosition, startPosition, fractionOfJourney);
+            wall.transform.localPosition = Vector3.Lerp(endPosition, startPosition, fractionOfJourney);
         }
     }
 
 
-    // Push trap
-    private IEnumerator PushTrap()
+    // On Trigger Enter
+    private void OnTriggerEnter(Collider other)
     {
-        yield return null;
-        trapCollider.enabled = true;
+        if (other.name.Contains("Player Model"))
+        {
+            if (!PlayerDeathController.GetDeathState())
+            {
+                Invoke("TriggerWall", 2f);
+            }
+        }
+    }
+
+
+    // Trigger Wall
+    private void TriggerWall()
+    {
         startTime = Time.time;
         journeyLength = Vector3.Distance(startPosition, endPosition);
         speed = pushSpeed;
         isRetract = false;
         isPush = true;
-        StartCoroutine(RetractTrap());
+        StartCoroutine(RetractWall());
     }
 
 
-    // Retract trap
-    private IEnumerator RetractTrap()
+    // Retract wall
+    private IEnumerator RetractWall()
     {
         yield return new WaitForSeconds(intervalInSeconds);
-        trapCollider.enabled = false;
         startTime = Time.time;
         journeyLength = Vector3.Distance(endPosition, startPosition);
         speed = retractSpeed;
         isPush = false;
         isRetract = true;
-        yield return new WaitForSeconds(intervalInSeconds);
-        StartCoroutine(PushTrap());
     }
 }
